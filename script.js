8a71ef650f61e690627a26cccd1d8801f2706874
bbokyou <bassmiyo@gmail.com>
2026-07-11 15:27:16 +0900

    API 연결 테스트

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("script.js 연결 성공!");
+const API_KEY = "test_93e40beacb1a3d3f59a5e0c5e736b7328932f2cbd9f0fb7f771ff5f7a0a87be3efe8d04e6d233bd35cf2fabdeb93fb0d";
+
+const url =
+  "https://open.api.nexon.com/mabinogi/v1/auction/list?auction_item_category=뷰티 쿠폰";
+
+async function loadAuction() {
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "x-nxopen-api-key": API_KEY
+            }
+        });
+
+        console.log("상태:", response.status);
+
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+loadAuction();
